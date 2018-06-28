68cfe12d4a0ec7cf70205c142d47c1371f2674e5
Leonardo Dutra <leo.togepi@gmail.com>
2018-06-28 12:52:35 -0300

    Initial commit

---
+++ functions.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <direct.h>
+#define LINHAS 24
+#define COLUNAS 81
+#define CANHAO 2
+#define navePOSX 40
+#define navePOSY 17
+#define naveDeltaVx 0
+#define tiroPOSX 40
+#define tiroPOSY 17
+#define tiroStatus 0
