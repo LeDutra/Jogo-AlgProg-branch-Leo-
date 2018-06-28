bb407734f9418e6060b26b83e5f187d16b979767
Leonardo Dutra <leo.togepi@gmail.com>
2018-06-28 20:13:10 -0300

    cbp updated
    sorry

--- files/main.c
+++ files/main.c
@@ -7,24 +7,30 @@ int main()
     int tempo, deltaTempo=0;
     int nave[3]= {navePOSX,navePOSY,naveDeltaVx};
     int tiro[3]= {tiroPOSX,tiroPOSY,tiroStatus};
+    int vidas = 3;
 
     tela_inicial();
     tempo=time(NULL);
-    while(deltaTempo<40)
+    for(vidas = 3; vidas != 0; vidas--)
     {
-        Sleep(20);
-        if(kbhit())
-            leitura_teclado(nave,tiro);
-        else
-            movimenta(nave,0);
-        atirar(nave,tiro);
-        posiciona_nave(nave,0);
-        deltaTempo=time(NULL)-tempo;
-        ajusta_energia(deltaTempo);
-        system("cls");
-        imprime_tela(deltaTempo);
-        if(deltaTempo == 35 || deltaTempo == 37 || deltaTempo == 39)
-            PlaySoundA(TEXT("sounds/alarm.wav"), NULL, SND_ASYNC);
+        while(deltaTempo<40)
+        {
+            Sleep(20);
+            if(kbhit())
+                leitura_teclado(nave,tiro);
+            else
+                movimenta(nave,0);
+            atirar(nave,tiro);
+            posiciona_nave(nave,0);
+            deltaTempo=time(NULL)-tempo;
+            ajusta_energia(deltaTempo);
+            system("cls");
+            imprime_tela(deltaTempo);
+            if(deltaTempo == 35 || deltaTempo == 37 || deltaTempo == 39)
+                PlaySoundA(TEXT("sounds/alarm.wav"), NULL, SND_ASYNC);
+        }
+        vidas--;
+        deltaTempo=0;
     }
     tela_game_over();
     return 0;
